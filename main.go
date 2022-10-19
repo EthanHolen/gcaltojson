24844585400953d199afbc834d06411042c80205
Ethan Holen <42720315+EthanHolen@users.noreply.github.com>
2022-10-19 11:24:25 -0600

    modified print

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ func ValidateWithGoogle(credentials string) {
 }
 
 func GetCalendarAsJson() {
-	fmt.Printf("converting calendar to json and returning \n")
+	fmt.Printf("converting calendar to json and returning.\n")
 }
